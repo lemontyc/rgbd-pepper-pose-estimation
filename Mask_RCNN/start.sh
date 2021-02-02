c2cb9cea79eae5eeaf502014bca4abbc694b3e8e
Luis Montoya <a01208334@itesm.mx>
2021-02-02 01:04:16 -0600

    [FIX] Print ports

--- Mask_RCNN/start.sh
+++ Mask_RCNN/start.sh
@@ -17,7 +17,6 @@ docker create \
 m_rcnn:0.1
 
 docker start m_rcnn
-docker port m_rcnn
 
 # Clone custom Mask-RCNN repo as the user of the host system
 cd $(pwd)/src
@@ -27,3 +26,5 @@ git clone https://github.com/lemontyc/Mask_RCNN.git
 docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn pip install -r requirements.txt
 docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn python setup.py install
 
+# Print ports
+docker port m_rcnn
