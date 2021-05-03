8dd557a4b1a6f83395b8e915a3363580f23c5238
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-05-03 11:14:40 -0500

    [FIX] Image works

--- Mask_RCNN/build_run.sh
+++ Mask_RCNN/build_run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Clone repo into src/
-mkdir -p src
-cd $(pwd)/src
+# Clone repo into a temporal folder. The google TF2 Dockerfile copies the repo from the temporal folder to the image
+# The tmeporal folder will be deleted to free space in the host device later
+mkdir -p tmp
+cd $(pwd)/tmp
 git clone https://github.com/lemontyc/models.git
 
 # Build docker image from the root of the git repository (inside the models directory)
 cd models
-docker build -f research/object_detection/dockerfiles/tf2/Dockerfile -t od .
+docker build \
+--file=research/object_detection/dockerfiles/tf2/Dockerfile \
+--tag="od:2.4.0" \
+.
+
+# Delete temporal folder
+cd ../..
+rm -rf tmp/
+
+
 # Run it
-docker run -it od
+docker run -it od:2.4.0
