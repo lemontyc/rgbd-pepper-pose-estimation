5b785e0d66c7621fe28fdc42d75a25cd0753f8bc
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-05-03 09:08:39 -0500

    [FEAT] Starting TF model library support

--- Mask_RCNN/build_run.sh
+++ Mask_RCNN/build_run.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
-# Build docker image
-docker build \
---file=tensorflow.Dockerfile \
---tag="m_rcnn:0.1" \
-.
-# Create folder that will bind to container
+
+# Clone repo into src/
 mkdir -p src
-# Create container with gpu capabilities
-docker create \
---gpus all \
---interactive \
---tty \
---rm \
---name=m_rcnn \
---publish-all \
---mount type=bind,source="$(pwd)/src",target=/home/pepper/GitHub \
---workdir /home/pepper/GitHub \
-m_rcnn:0.1
-
-docker start m_rcnn
-
-# Clone custom Mask-RCNN repo as the user of the host system
 cd $(pwd)/src
-git clone https://github.com/lemontyc/Mask_RCNN.git
+git clone https://github.com/lemontyc/models.git
 
-# And install it
-docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn pip install -r requirements.txt
-docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn python setup.py install
-
-# Print ports
-docker port m_rcnn
+# Build docker image from the root of the git repository (inside the models directory)
+cd models
+docker build -f research/object_detection/dockerfiles/tf2/Dockerfile -t od .
+# Run it
+docker run -it od
