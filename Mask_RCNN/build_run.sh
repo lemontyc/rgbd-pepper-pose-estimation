f77ab545c304f21aa1b952446760a04441e68d97
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-05-03 12:17:54 -0500

    [FIX] Shared volume on TF container

--- Mask_RCNN/build_run.sh
+++ Mask_RCNN/build_run.sh
@@ -2,8 +2,8 @@
 
 # Clone repo into a temporal folder. The google TF2 Dockerfile copies the repo from the temporal folder to the image
 # The tmeporal folder will be deleted to free space in the host device later
-mkdir -p tmp
-cd $(pwd)/tmp
+mkdir -p volume
+cd $(pwd)/volume
 git clone https://github.com/lemontyc/models.git
 
 # Build docker image from the root of the git repository (inside the models directory)
@@ -13,10 +13,17 @@ docker build \
 --tag="od:2.4.0" \
 .
 
-# Delete temporal folder
+# Move outside volume/
 cd ../..
-rm -rf tmp/
 
+# Create container with gpu capabilities
+docker create \
+--gpus all \
+--interactive \
+--tty \
+--rm \
+--name=m_rcnn \
+--mount type=bind,source="$(pwd)/volume/models",target=/home/tensorflow/models \
+od:2.4.0
 
-# Run it
-docker run -it od:2.4.0
+docker start m_rcnn
--- Mask_RCNN/start.sh
+++ Mask_RCNN/start.sh
@@ -6,16 +6,7 @@ docker create \
 --tty \
 --rm \
 --name=m_rcnn \
---publish-all \
---mount type=bind,source="$(pwd)/src",target=/home/pepper/GitHub \
---workdir /home/pepper/GitHub \
-m_rcnn:0.1
+--mount type=bind,source="$(pwd)/volume/models",target=/home/tensorflow/models \
+od:2.4.0
 
 docker start m_rcnn
-
-# And install it
-docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn pip install -r requirements.txt
-docker exec  -w /home/pepper/GitHub/Mask_RCNN m_rcnn python setup.py install
-
-# Print ports
-docker port m_rcnn
