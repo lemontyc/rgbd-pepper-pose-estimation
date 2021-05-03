703aac948ac38af232fe4c80c1edc21150c5ba72
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-05-03 10:51:18 -0500

    [FIX] Image generation failed

--- Mask_RCNN/build_run.sh
+++ Mask_RCNN/build_run.sh
@@ -6,6 +6,7 @@ cd $(pwd)/src
 git clone https://github.com/lemontyc/models.git
 
 # Build docker image from the root of the git repository (inside the models directory)
-docker build -f models/research/object_detection/dockerfiles/tf2/Dockerfile -t od .
+cd models
+docker build -f research/object_detection/dockerfiles/tf2/Dockerfile -t od .
 # Run it
 docker run -it od
